57363e90e03b14cf4f112693a0f09d1a62db5c73
saad zafar <codewithsaadzafar@gmail.com>
2025-01-28 08:46:26 +0500

    Navbar completed, todo list styling started

---
+++ components/Nav.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+import { useState, useEffect } from "react"
+import styles from '../styles/Nav.module.css'
+import logo from '../public/images/logo.svg'
+
+export default function Nav() {
+    return (
+        <div className={styles.navMain}>
+            <img src={logo.src} alt="" />
+            <ul >
+                <li className = {styles.navItem}>
+                    <a href="">Home</a>
+                </li>
+                <li className = {styles.navItem}>
+                    <a href="">Add Todo</a>
+                </li>
+                <li className = {styles.navItem}>
+                    <a href="">Logout</a>
+                </li>
+                <li className = {styles.navItem}>
+                    <a href="">About App</a>
+                </li>
+            </ul>
+
+        </div>
+    )
+}
---
+++ components/Todos.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import styles from '../styles/Todo.module.css'
+
+export default function Todos(){
+    return (
+        <div className = {styles.todoMain}>
+            <h1>Todo List</h1>
+        </div>
+    )
+}
